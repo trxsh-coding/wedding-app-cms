3731a430b612164646620c210cab57649f621f62
Orest Knxwn <orest@200apps.com>
2025-05-03 13:44:02 +0300

    first commit

---
+++ src/api/qr-code/controllers/qr-code.ts
@@ -0,0 +1,7 @@
+/**
+ * qr-code controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::qr-code.qr-code');
---
+++ src/api/qr-code/services/qr-code.ts
@@ -0,0 +1,7 @@
+/**
+ * qr-code service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::qr-code.qr-code');
